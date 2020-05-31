e1b5d21ee0f7148b7fb82a3810a872b581897a57
leon <leon@9demensions.io>
2020-05-31 13:02:18 +0800

    feat(): default login page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+   <v-card width="400px" class="mt-5 mx-auto">
+     <v-card-title>
+      <h1 class="display-1">Login</h1>
+     </v-card-title>
+       <v-card-text>
+         <v-form>
+            <v-text-field prepend-icon="mdi-account" clearable label="user name"> 
+            </v-text-field>
+            <v-text-field
+                :type="showPAssword ? 'text' : 'password'"
+                label="user password -> Bobo's birthday"
+                prepend-icon="mdi-lock"
+                :append-icon="showPAssword ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="showPAssword = !showPAssword"
+                />
+           
+         </v-form>
+       </v-card-text>
+       <v-divider></v-divider>
+       <v-card-actions>
+         <v-btn color="success">Register</v-btn>
+         <v-spacer></v-spacer>
+         <v-btn color="info">Login</v-btn>
+       </v-card-actions>
+   </v-card>
+  </v-app>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      showPAssword: false
+    }
+  },
   components: {
-    HelloWorld
-  }
-}
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
